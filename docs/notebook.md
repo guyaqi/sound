4bb2b7f18bf09357c2ebaa52beddeb0b3a5f4c6d
ubuntu <ubuntu@localhost.localdomain>
2021-02-04 22:52:55 +0800

    a

---
+++ docs/notebook.md
@@ -0,0 +1,15 @@
+# notebook
+
+python 音频分析
+
+## librosa
+
+```python
+librosa.display.waveplot(y, sr) # 绘制波形图
+librosa.display.specshow(logmelspec, sr=sr, x_axis='time', y_axis='mel') # 绘制频谱图
+```
+
+## 术语
+
+* Mel频率倒谱系数
+* FFT：快速傅立叶变换，是离散傅氏变换(DFT)的快速算法
--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-import scipy
+import librosa
+import librosa.display
+from matplotlib import pyplot as plt
+import soundfile
 
+import logging
+logging.basicConfig(level=logging.NOTSET)
+
+
+y, sr = librosa.load('./data/long_001.mp3', sr=None)
+logging.info(f'采样率: {sr}')
+
+# show waveform
+def gen_waveform():
+  plt.figure()
+
+  # use waveplot to gen waveform
+  librosa.display.waveplot(y, sr)
+
+  plt.title('waveform')
+  plt.savefig("./out/waveform.png")
+
+def gen_specs():
+  melspec = librosa.feature.melspectrogram(y, sr, n_fft=1024, hop_length=512, n_mels=128)
+  logmelspec = librosa.power_to_db(melspec)
+  plt.figure()
+  librosa.display.specshow(logmelspec, sr=sr, x_axis='time', y_axis='mel')
+  plt.title('spec')
+  plt.savefig("./out/spec.png")
+
+def gen_sound():
+  para = 3
+  weight = 1/para
+
+  total_len = len(y)
+  priece = int(total_len/para)
+  new_y = y[0: priece] * 0
+  for i in range(para):
+    new_y += y[priece * i: priece * (i+1)] * weight
+  
+  soundfile.write('./out/1.wav', new_y, sr)
+
+# gen_waveform()
+# gen_specs()
+gen_sound()
